e21692c8587c2e94caa23b668ded54794b7d505b
Braeden <bclarke@fastmail.com>
2024-06-22 10:07:22 -0500

    Initial commit

---
+++ electron/main.cjs
@@ -0,0 +1,54 @@
+const { log } = require("console");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDevEnvironment = process.env.DEV_ENV === "true";
+
+// enable live reload for electron in dev mode
+if (isDevEnvironment) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "..", "node_modules", ".bin", "electron"),
+    hardResetMethod: "exit",
+  });
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // define how electron will load the app
+  if (isDevEnvironment) {
+    mainWindow.loadURL("http://localhost:5173/");
+
+    log("Electron running in dev mode: 🧪");
+  } else {
+    // when not in dev mode, load the build file instead
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
+
+    log("Electron running in prod mode: 🚀");
+  }
+};
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+// app.on('window-all-closed', () => {
+//     if (process.platform !== 'darwin') app.quit()
+// })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
