e277a69044661e9ef3d52c6c397883bca8fa1561
Braeden <bclarke@fastmail.com>
2024-06-25 16:16:53 -0500

    Add simulator to global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,8 @@
+import type Simulator from "./src/logic/simulator/simulator";
+
 declare global {
     var editor: import("monaco-editor").editor.IStandaloneCodeEditor;
 }
 
 var editor: import("monaco-editor").editor.IStandaloneCodeEditor;
+var simulator: Simulator
